0f465fc78020ff02349711c578f0e6886a79efa4
unknown <juanse-14@hotmail.com>
2015-11-03 19:32:08 -0500

    Se agregaron comentarios sobre los eventos del tablero braille

--- Braill'App/app/src/main/java/com/project/rubikon/braillapp/BrailleScreen.java
+++ Braill'App/app/src/main/java/com/project/rubikon/braillapp/BrailleScreen.java
@@ -1,5 +1,6 @@
 package com.project.rubikon.braillapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -33,5 +34,42 @@ public class BrailleScreen extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void pasarTweet(){
+
+        // va a haber una variable bandera que va a indicar si esta la pantalla bloqueada
+        // para realizar movimientos de desplazamiento o esta desbloqueada
+        // True representa que si se pueden realizar movimientos
+
+        if(true){
+            // aqui se especifica hacia donde se dirige el movimiento
+            // dependiendo de la informacion brindada por la accion realizada por el usuario
+            // puede cambiar la informacion si va hacia arriba sigue leyendo el tweet
+            // si va hacia la izquierda lee el siguiente tweet, si va a la derecha
+            // pasa al anterior tweet y si va hacia abajo vuelve a la parte superior del tweet que
+            // esta leyendo
+            // Si al hacer un movimiento buscando un tweet nuevo no se encuentran mas tweets por leer
+            // Se cambia la bandera de Si hay mas tweets por leer a TRUE y entra al if que
+            // Cambia a la activity de cargando tweets.....
+
+
+            // Este seria el if de la bandera que indica que no hay mas tweets por leer
+            //
+            if(true){
+
+
+                Intent newfront = new Intent(BrailleScreen.this, CargaTweets.class);
+                startActivity(newfront);
+            }
+
+
+            Intent newfront = new Intent(BrailleScreen.this, BrailleScreen.class);
+            startActivity(newfront);
+        }
+
+
+
+
     }
 }
