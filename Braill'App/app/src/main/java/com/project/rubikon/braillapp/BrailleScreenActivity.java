8fe29c2103debc05ae75dd480f21df7b58067cd3
jersi2695 <jersi2695@hotmail.com>
2015-11-25 14:10:23 -0500

    App funcional sin audio

--- Braill'App/app/src/main/java/com/project/rubikon/braillapp/BrailleScreenActivity.java
+++ Braill'App/app/src/main/java/com/project/rubikon/braillapp/BrailleScreenActivity.java
@@ -45,8 +45,6 @@ public class BrailleScreenActivity extends Activity implements GestureDetector.O
 
     private static int defaultStates[];
     private GestureDetector gd;
-    private static Tweet tweety;
-    private static String[] listTweets;
     private static ArrayList[] listaTweets = new ArrayList[20];
     private String segment = "1000101010001010100101101011010100001010100000110001010101";
     int tweetCounter = 0; //Columnas del array de arraylist que es un tweet con su lista de paginas
@@ -67,43 +65,27 @@ public class BrailleScreenActivity extends Activity implements GestureDetector.O
         gestures = new GestureDetectorCompat(this, this);
         gestures.setOnDoubleTapListener(this);
 
-        Thread closeActivity = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
+        String[] tweets =  new String[20];
+        tweets = getIntent().getStringArrayExtra("Tweets");
+        for (int i = 0; i < tweets.length; i++) {
+            Tweet tweety = new Tweet(tweets[i]);
+            listaTweets[i] = tweety.getTraslation();
+        }
 
-                    Thread.sleep(10000);
-                    // Do some stuff
-                } catch (Exception e) {
-                    e.getLocalizedMessage();
-                }
-            }
-        });
-        TweetRepository.getInstance().getTimelineAsync(timelineListener); // => timelineListener
-        //updateBrailleScreen((String) listaTweets[tweetCounter].get(segmentCounter));
-
-//        gd = new GestureDetector(this);
+        updateBrailleScreen((String) listaTweets[0].get(0));
 //
-//        //set the on Double tap listener
-//        gd.setOnDoubleTapListener(new OnDoubleTapListener(){
+//        Thread closeActivity = new Thread(new Runnable() {
 //            @Override
-//            public boolean onSingleTapConfirmed(MotionEvent e) {
-//                return false;
-//            }
+//            public void run() {
+//                try {
 //
-//            @Override
-//            public boolean onDoubleTap(MotionEvent e) {
-//                lock=!lock;
-//                Log.d("doubleTap", lock+"");
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean onDoubleTapEvent(MotionEvent e) {
-//                return false;
+//                    Thread.sleep(10000);
+//                    // Do some stuff
+//                } catch (Exception e) {
+//                    e.getLocalizedMessage();
+//                }
 //            }
 //        });
-
 
         final View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
@@ -114,13 +96,10 @@ public class BrailleScreenActivity extends Activity implements GestureDetector.O
             }
         };
 
-
         View.OnTouchListener t = new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View view, MotionEvent event) {
-
-//                gd.onTouchEvent(event);
 
                 int x = (int) event.getX();
                 int y = (int) event.getY();
@@ -128,47 +107,6 @@ public class BrailleScreenActivity extends Activity implements GestureDetector.O
 
 
                 if(lock) {
-////                    switch (event.getActionMasked()){
-////
-////                        case MotionEvent.ACTION_DOWN:{
-////                            x1 = event.getX();
-////                            y1 = event.getY();
-////                            break;
-////                        }
-////
-////                        case MotionEvent.ACTION_UP: {
-////                            float x2 = event.getX();
-////                            float y2 = event.getY();
-////                            float diffX = x2 - x1;
-////                            float diffY = y2 - y1;
-////
-////                            if(Math.abs(diffX) > Math.abs(diffY))
-////                            {
-////                                if(diffX > 0) {
-////                                    prevTweet();
-////                                    Log.d("TAG-RUBIKON", "Left");
-////                                }else {
-////                                    nextTweet();
-////                                    Log.d("TAG-RUBIKON", "Right");
-////                                }
-////                            }
-////                            else
-////                            {
-////                                if(diffY > 0){
-////                                    prevPage();
-////                                    Log.d("TAG-RUBIKON", "Up");
-////                                } else{
-////                                    nextPage();
-////                                    Log.d("TAG-RUBIKON", "Down");
-////                                }
-////
-////                            }
-////                            break;
-////                        }
-////                    }
-////                }
-////                else
-////                {
                     for (int i = 0; i < touchview.getChildCount(); i++) {
                         View current = touchview.getChildAt(i);
                         if (current instanceof ImageButton) {
@@ -196,16 +134,7 @@ public class BrailleScreenActivity extends Activity implements GestureDetector.O
             }
         };
         touchview.setOnTouchListener(t);
-
-
     }
-
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        gestures.onTouchEvent(event);
-//
-//        return super.onTouchEvent(event);
-//    }
 
     public void updateBrailleScreen(String segment) {
 
@@ -235,40 +164,6 @@ public class BrailleScreenActivity extends Activity implements GestureDetector.O
         return (x <= x2 && x >= x1 && y <= y2 && y >= y1);
     }
 
-
-    public void pasarTweet() {
-
-        // va a haber una variable bandera que va a indicar si esta la pantalla bloqueada
-        // para realizar movimientos de desplazamiento o esta desbloqueada
-        // True representa que si se pueden realizar movimientos
-
-        if (true) {
-            // dependiendo de la informacion brindada por la accion realizada por el usuario
-            // puede cambiar la informacion si va hacia arriba sigue leyendo el tweet
-            // aqui se especifica hacia donde se dirige el movimiento
-            // si va hacia la izquierda lee el siguiente tweet, si va a la derecha
-            // pasa al anterior tweet y si va hacia abajo vuelve a la parte superior del tweet que
-            // esta leyendo
-            // Si al hacer un movimiento buscando un tweet nuevo no se encuentran mas tweets por leer
-            // Se cambia la bandera de Si hay mas tweets por leer a TRUE y entra al if que
-            // Cambia a la activity de cargando tweets.....
-
-
-            // Este seria el if de la bandera que indica que no hay mas tweets por leer
-            //
-            if (true) {
-
-
-                Intent newfront = new Intent(BrailleScreenActivity.this, LoadTweetsActivity.class);
-                startActivity(newfront);
-            }
-
-
-            Intent newfront = new Intent(BrailleScreenActivity.this, BrailleScreenActivity.class);
-            startActivity(newfront);
-        }
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -290,40 +185,6 @@ public class BrailleScreenActivity extends Activity implements GestureDetector.O
 
         return super.onOptionsItemSelected(item);
     }
-
-    private void showTimeline(ResponseList<Status> statuses) {
-        // Creamos un array de Strings con el texto de los Status( Tweets )
-        String[] tweets = new String[statuses.size()];
-        int counter = 0;
-        for (Status status : statuses) {
-            tweets[counter] = status.getText();
-            tweety = new Tweet(tweets[counter].split("http")[0].toString());
-            listaTweets[counter] = tweety.getTraslation();
-            Log.d("TAG-RUBIKON", "Tweets: " + tweets[counter].split("http")[0].toString());
-            counter++;
-        }
-        updateBrailleScreen((String) listaTweets[0].get(0));
-    }
-
-    // Esta clase interna es la encargada de manejar el callback,  tiene dos m�todos para manejar la posibilidad de �xito y de error.
-    TwitterListener timelineListener = new TwitterAdapter() {
-
-        @Override
-        public void gotHomeTimeline(ResponseList<Status> statuses) {
-            showTimeline(statuses);
-        }
-
-        @Override
-        public void onException(TwitterException te, TwitterMethod method) {
-            //showError();
-        }
-
-        @Override
-        public void gotUserTimeline(ResponseList<Status> statuses) {
-            showTimeline(statuses);
-        }
-    };
-
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent e) {
@@ -404,37 +265,9 @@ public class BrailleScreenActivity extends Activity implements GestureDetector.O
                 }
 
             }
-//        } else {
-//            Log.d("Swipe", "Locked Screen");
-//            int x = (int) e2.getX();
-//            int y = (int) e2.getY();
-//
-//            for (int i = 0; i < touchview.getChildCount(); i++) {
-//                View current = touchview.getChildAt(i);
-//                if (current instanceof ImageButton) {
-//                    ImageButton b = (ImageButton) current;
-//
-//                    if (!isPointWithin(x, y, b.getLeft(), b.getRight(), b.getTop(),
-//                            b.getBottom())) {
-//                        b.getBackground().setState(defaultStates);
-//                    }
-//
-//                    if (isPointWithin(x, y, b.getLeft(), b.getRight(), b.getTop(),
-//                            b.getBottom())) {
-//                        b.getBackground().setState(STATE_PRESSED);
-//                        if ((int) b.getTag() == R.drawable.filled13) {
-//                            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-//                            vibrator.vibrate(100);
-//                        }
-//                    }
-//
-//                }
-//            }
         }
         return true;
     }
-
-
 
     public void prevPage(){
         if(segmentCounter>0)
@@ -459,8 +292,12 @@ public class BrailleScreenActivity extends Activity implements GestureDetector.O
     }
 
     public void nextTweet() {
-        if(tweetCounter<20)
+        if(tweetCounter<19)
             tweetCounter++;
+        else{
+            Intent newfront = new Intent(BrailleScreenActivity.this, LoadTweetsActivity.class);
+            startActivity(newfront);
+        }
         segmentCounter = 0;
         ArrayList currentTweet = listaTweets[tweetCounter];
         updateBrailleScreen((String) currentTweet.get(segmentCounter));
--- Braill'App/app/src/main/java/com/project/rubikon/braillapp/LoadTweetsActivity.java
+++ Braill'App/app/src/main/java/com/project/rubikon/braillapp/LoadTweetsActivity.java
@@ -20,6 +20,7 @@ import twitter4j.TwitterMethod;
 
 public class LoadTweetsActivity extends AppCompatActivity {
     private boolean flag= false;
+    private String[] tweets = new String[20];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +54,46 @@ public class LoadTweetsActivity extends AppCompatActivity {
     private static Tweet tweety;
     private static ArrayList[] listaTweets= new ArrayList[20];
 
+    private void showTimeline(ResponseList<Status> statuses) {
+        // Creamos un array de Strings con el texto de los Status( Tweets )
+        tweets = new String[statuses.size()];
+        int counter = 0;
+        for (Status status : statuses) {
+            tweets[counter] = status.getText().split("http")[0];
+//            tweety = new Tweet(tweets[counter].split("http")[0].toString());
+//            listaTweets[counter] = tweety.getTraslation();
+//            Log.d("TAG-RUBIKON", "Tweets: " + tweets[counter].split("http")[0].toString());
+            counter++;
+        }
+
+        if(tweets.length>0){
+            Intent newfront = new Intent(LoadTweetsActivity.this, BrailleScreenActivity.class);
+            newfront.putExtra("Tweets",tweets);
+            startActivity(newfront);
+        }else{
+            Intent newfront = new Intent(LoadTweetsActivity.this, NoTweetsActivity.class);
+            startActivity(newfront);
+        }
+    }
+
+    // Esta clase interna es la encargada de manejar el callback,  tiene dos m?todos para manejar la posibilidad de ?xito y de error.
+    TwitterListener timelineListener = new TwitterAdapter() {
+
+        @Override
+        public void gotHomeTimeline(ResponseList<Status> statuses) {
+            showTimeline(statuses);
+        }
+
+        @Override
+        public void onException(TwitterException te, TwitterMethod method) {
+            //showError();
+        }
+
+        @Override
+        public void gotUserTimeline(ResponseList<Status> statuses) {
+            showTimeline(statuses);
+        }
+    };
 
     public void loadingTweets(){
         //Aqui va el evento de cuando se cargan los tweets, una vez ya cargados el pasa a la activity
@@ -61,15 +102,6 @@ public class LoadTweetsActivity extends AppCompatActivity {
         // y pasa a la activity de lectura, si no existen tweets para cargar el retorno es false
         // se pasa a la activity de no hay tweets.
 
-
-        if(true){
-
-            Intent newfront = new Intent(LoadTweetsActivity.this, BrailleScreenActivity.class);
-            startActivity(newfront);
-        }else{
-            Intent newfront = new Intent(LoadTweetsActivity.this, NoTweetsActivity.class);
-            startActivity(newfront);
-        }
-
+        TweetRepository.getInstance().getTimelineAsync(timelineListener); // => timelineListener
     }
 }
